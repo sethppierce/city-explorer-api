4da670a522ab28aba500c2c38796e07b3c8e8a8e
Seth <sethparkerpierce@icloud.com>
2022-10-26 13:03:10 -0400

    unrefactoring, changes broke code

--- server.js
+++ server.js
@@ -6,33 +6,34 @@ require('dotenv').config();
 let weatherData = require('./data/weather.json');
 const app = express();
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3002
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', (req, res)  => {
   res.status(200).send('Welcome to my server!');
 });
 
 
 app.get('/weather', (req, res, next) => {
-
+  
   try {
-    const { lat, lon} = req.query;
-    const cityName = req.query.city_name;
+    const { lat, lon, ...rest} = req.query
+    const cityName = req.query?.city_name
     const dataToGroom = weatherData.find(city => {
-      return Math.floor(city.lat) === Math.floor(lat) && Math.floor(city.lon) === Math.floor(lon);
-    });
+      return Math.floor(city?.lat) === Math.floor(lat) && Math.floor(city?.lon) === Math.floor(lon)
+    })
+    
     const weatherObj = new CityWeather(dataToGroom);
     const arrayToGroom = weatherObj.data.map(day => {
       return ({
         date: day.datetime,
         description: `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`
-      });
-    });
+      })
+    })
     const dataToSend = {
       description: arrayToGroom
-    };
+    }
     res.status(200).send(dataToSend);
   } catch (error) {
     next(error);
@@ -43,9 +44,9 @@ app.get('/weather', (req, res, next) => {
 
 class CityWeather {
   constructor(cityData) {
-    this.lon = cityData.lon;
-    this.lat = cityData.lat;
-    this.data = [...cityData.data];
+    this.lon = cityData.lon
+    this.lat = cityData.lat
+    this.data = [...cityData.data]
   }
 }
 
@@ -56,12 +57,12 @@ app.get('*', (req, res) => {
   res.status(404).send('This route is not found');
 });
 
-app.use((e, req, res) => {
+app.use((e, req, res, next) => {
   res.status(500).send(`${req.query.city_name} not found!!!
   ${e}`);
 });
 
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`)
+})
