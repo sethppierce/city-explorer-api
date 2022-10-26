84810783050e1da4f763bad799f32be0c7194090
Seth <sethparkerpierce@icloud.com>
2022-10-26 01:33:53 -0400

    finished lab07

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ let weatherData = require('./data/weather.json')
 
 const port = process.env.PORT || 3002
 
-
-
-/* MIDDLEWARE */
-
 app.use(cors());
 
 app.get('/', (req, res)  => {
@@ -19,7 +15,6 @@ app.get('/', (req, res)  => {
 });
 
 
-/* PATHS */
 app.get('/weather', (req, res, next) => {
   
   try {
@@ -30,53 +25,47 @@ app.get('/weather', (req, res, next) => {
       return Math.floor(city?.lat) === Math.floor(lat) && Math.floor(city?.lon) === Math.floor(lon)
     }
 
-
     )
-    const dataObject = new Forecast(dataToGroom);
-    console.log(dataObject)
-    const arrayToGroom = dataObject.data.map(day => {
+    const dataObj = new CityWeather(dataToGroom);
+    console.log(dataObj)
+    const arrayToGroom = dataObj.data.map(day => {
       return ({
         date: day.datetime,
         description: `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`
-
       })
     })
     const dataToSend = {
       description: arrayToGroom
     }
-
     res.status(200).send(dataToSend);
   } catch (error) {
-    // if I have an error, this will create a new instance of the Error Object that lives in Express
     next(error);
   }
 });
 
 
 
-
-app.get('*', (req, res) => {
-  res.status(404).send('This route does not exist');
-});
-
-
-class Forecast {
+class CityWeather {
   constructor(cityData) {
-    this.data = [...cityData.data]
     this.lon = cityData.lon
     this.lat = cityData.lat
+    this.data = [...cityData.data]
   }
 }
 
 
-/* ERROR HANDLE */
+//  Errors
+
+app.get('*', (req, res) => {
+  res.status(404).send('This route is not found');
+});
 
 app.use((e, req, res, next) => {
-  res.status(500).send(`${req.query.city_name} was not found!
+  res.status(500).send(`${req.query.city_name} not found!!!
   ${e}`);
 });
 
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
